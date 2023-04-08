da994df91ea4b79ee4ffc0a1ad06e19e30646efe
sanju2728 <sanjanabhat2002@gmail.com>
2023-04-08 14:57:00 +0530

    minor changes

--- botenv/pics.py
+++
@@ -1,6 +0,0 @@
-import requests
-res = requests.get('https://api.thecatapi.com/v1/images/search?limit=50&api_key=live_hsdgsb1mW4QfRrGZt6I0IXfmAecBn5hAqhdcaTJoTr260mR4DwiJ14rdyp62gLI4')
-if(res.status_code == 200):
-    cats = res.json()
-    for cat in cats:
-        print(cat["url"])
